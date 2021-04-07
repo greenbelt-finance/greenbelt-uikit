4be48d5c97e3c57441582a534f1fb0f014a9344a
greenbelt-dev <grnbeltfinance@gmail.com>
2021-04-07 16:31:16 +0900

    Update color code

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -2,10 +2,10 @@ import { Colors } from "./types";
 
 export const baseColors = {
   failure: "#ED4B9E",
-  primary: "#aa8929",
-  primaryBright: "#aa8929",
-  primaryDark: "#aa8929",
-  secondary: "#7645D9",
+  primary: "#00AE74",
+  primaryBright: "#00AE74",
+  primaryDark: "#00AE74",
+  secondary: "#007A5E",
   success: "#31D0AA",
   warning: "#FFB237",
 };
@@ -23,9 +23,9 @@ export const lightColors: Colors = {
   invertedContrast: "#FFFFFF",
   input: "#eeeaf4",
   tertiary: "#EFF4F5",
-  text: "#452A7A",
-  textDisabled: "#BDC2C4",
-  textSubtle: "#8f80ba",
+  text: "#054A3A",
+  textDisabled: "#61716A",
+  textSubtle: "#09DB90",
   borderColor: "#E9EAEB",
   card: "#FFFFFF",
   gradients: {
@@ -36,7 +36,7 @@ export const lightColors: Colors = {
 export const darkColors: Colors = {
   ...baseColors,
   ...brandColors,
-  secondary: "#9A6AFF",
+  secondary: "#09DB90",
   background: "#343135",
   backgroundDisabled: "#3c3742",
   contrast: "#FFFFFF",
@@ -44,9 +44,9 @@ export const darkColors: Colors = {
   input: "#483f5a",
   primaryDark: "#0098A1",
   tertiary: "#353547",
-  text: "#EAE2FC",
-  textDisabled: "#666171",
-  textSubtle: "#c9c4d4",
+  text: "#ffffff",
+  textDisabled: "#61716A",
+  textSubtle: "#007A5E",
   borderColor: "#524B63",
   card: "#27262c",
   gradients: {
