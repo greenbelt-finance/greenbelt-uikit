e6929c0bf00669f917df4b841a318ebd3806c31b
greenbelt-dev <grnbeltfinance@gmail.com>
2021-04-14 16:28:05 +0900

    Remove not used link

--- src/widgets/Menu/config.ts
+++ src/widgets/Menu/config.ts
@@ -126,10 +126,6 @@ export const socials = [
       //   label: "Bahasa Indonesia",
       //   href: "https://t.me/PancakeSwapIndonesia",
       // },
-      {
-        label: "中文",
-        href: "https://t.me/goosefinancechinese",
-      },
       // {
       //   label: "Tiếng Việt",
       //   href: "https://t.me/PancakeSwapVN",
@@ -138,38 +134,6 @@ export const socials = [
       //   label: "Italiano",
       //   href: "https://t.me/pancakeswap_ita",
       // },
-      {
-        label: "русский",
-        href: "https://t.me/goosefinancerussian",
-      },
-      {
-        label: "Türkiye",
-        href: "https://t.me/GooseFinanceTurkey",
-      },
-      // {
-      //   label: "Português",
-      //   href: "https://t.me/PancakeSwapPortuguese",
-      // },
-      {
-        label: "Español",
-        href: "https://t.me/goosefinancespanish",
-      },
-      {
-        label: "日本語",
-        href: "https://t.me/goosefinancejapanese",
-      },
-      // {
-      //   label: "Français",
-      //   href: "https://t.me/pancakeswapfr",
-      // },
-      {
-        label: "Announcements",
-        href: "https://t.me/goosefinanceann",
-      },
-      {
-        label: "Price Bot",
-        href: "https://t.me/BinanceRocketEGG",
-      },
     ],
   },
   {
